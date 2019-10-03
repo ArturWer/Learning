ae944269ca12ca57ac84a1a7c237ac0c3c13a6cf
Arthur Vershyhora <ArturiTunes@gmail.com>
2019-10-03 20:54:01 +0300

    added chessBoard exercise

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,7 @@ for (let i = 0; i < 7; i++) {
 	text += "#";
 }*/
 
-/* fizz buzz */
+/* fizz buzz 
 let msg = 1;
 for (var i = 0; i < 100; i++) {
 	if (msg % 3 === 0 && msg % 5 === 0) 
@@ -18,4 +18,23 @@ for (var i = 0; i < 100; i++) {
 	else 
 	  console.log(msg);
 	msg++;
+}*/
+
+/* chess board */
+let board = "", nextSymbol = "#", column = 0;
+for (let i = 0; i < 64; i++) {
+	board += nextSymbol;
+	if (column >= 7){
+		board += `\n${nextSymbol}`;
+		column = 1;
+	} else 	column++;
+	
+	if (nextSymbol === "#") 
+		nextSymbol = "_";
+	else nextSymbol = "#";
+
+
 }
+console.log(board);
+
+/**/
